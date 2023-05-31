475a41a25329464f97aa443f638f948e35923808
Felipe Mata <felipe_motoss@hotmail.com>
2023-05-30 18:19:52 -0600

    validating the user input, errorForm and must be a valid...

--- app.js
+++ app.js
@@ -14,16 +14,41 @@ form.addEventListener('submit', (e) => {
     const regexMonth = /^(0?[1-9]|1[0-2])$/;
     const regexYear = /^[0-9]{4}$/;
 
-    if (regexDay.test(day) && regexMonth.test(month) && regexYear.test(year)) {
+    const calculatedYear = document.querySelector('#calculatedYear');
+    const calculatedMonth = document.querySelector('#calculatedMonth');
+    const calculatedDay = document.querySelector('#calculatedDay');
+
+    const currentDate = new Date();
+
+    const errorForm = document.querySelector('#errorForm');
+    const errorDay = document.querySelector('#errorDay');
+    const errorMonth = document.querySelector('#errorMonth');
+    const errorYear = document.querySelector('#errorYear');
+
+
+    if (!regexDay.test(day)) {
+        errorDay.classList.remove('inactive');
+    }
+
+    if (!regexMonth.test(month)) {
+        errorMonth.classList.remove('inactive');
+    }
+
+    if (!regexYear.test(year)) {
+        errorYear.classList.remove('inactive');
+    }
+
+    if (regexDay.test(day) && regexMonth.test(month) && (regexYear.test(year) && year < currentDate.getFullYear())) {
         console.log('success');
         // Proceed with further processing or validation
 
         const birthDate = new Date(year, month - 1, day);
-        const currentDate = new Date();
+        // const currentDate = new Date();
 
         let ageYears = currentDate.getFullYear() - birthDate.getFullYear();
         let ageMonths = currentDate.getMonth() - birthDate.getMonth();
         let ageDays = currentDate.getDate() - birthDate.getDate();
+
 
         // Check if current day is smaller than birth day
         if (ageDays < 0) {
@@ -41,9 +66,14 @@ form.addEventListener('submit', (e) => {
         }
 
         console.log(`Age: ${ageYears} years, ${ageMonths} months, ${ageDays} days`);
+
     } else {
         console.log('Invalid input. Please enter two digits (e.g., 01, 25).');
         // Display an error message or take appropriate action
+
+        errorForm.classList.remove('inactive');
+
+
     }
 
 })
